4aa8fb39da91d0123505328555b574b456475226
Michelle Miller <chellimiller@users.noreply.github.com>
2022-01-27 20:57:20 -0700

    Add css-mixin script (comments only)

---
+++ scripts/generators/css-mixins.js
@@ -0,0 +1,10 @@
+require('csstype');
+
+/// Read in CSS properties
+/// Convert to Sass mixins
+/// All property mixins will use css-property-with-token
+/// Write to src/_css.scss
+
+
+/// Probably add generator for index.scss
+/// Possibly add this to build only
