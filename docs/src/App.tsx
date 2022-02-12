02218a4ae42205abe9ff0900ea8f18136d2c161a
Michelle Miller <chellimiller@users.noreply.github.com>
2022-02-12 15:19:29 -0700

    #2 Initialize docs

---
+++ docs/src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { hot } from "react-hot-loader/root";
+
+const App: React.FC = () => (
+  <div id="app">
+    <h1>Hello</h1>
+  </div>
+);
+
+export default hot(App);
